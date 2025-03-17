acd561dd760e9f4c47ccbf2df61af427f3076b55
Alan Carlos <alan.add34@gmail.com>
2025-03-17 10:24:02 -0300

    Update

---
+++ script/script.js
@@ -0,0 +1,7 @@
+let aceito = document.querySelector("#aceito")
+let negado = document.querySelector("#negado")
+aceito.addEventListener('click',()=>{
+    console.log()
+})
+negado.addEventListener('click',()=>{
+})
