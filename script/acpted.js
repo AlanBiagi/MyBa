97653dbd6bfde88a0b3ac6fd567a472a4c82dc0b
Alan Carlos <alan.add34@gmail.com>
2025-03-17 14:10:59 -0300

    Update-4

--- script/acpted.js
+++ script/acpted.js
@@ -1,6 +1,6 @@
-i = 0
+i = 1
 if(i == 0){setTimeout(() => {
     
             window.document.location.href = location.href ="https://www.youtube.com/shorts/XfRJxEMk_90?feature=share"
-}, 4000);
+}, 6000);
 }
