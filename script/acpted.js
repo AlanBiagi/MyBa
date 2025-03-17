23ea978cd7b2ee49caa11353fe5fc65e70eb0df2
Alan Carlos <alan.add34@gmail.com>
2025-03-17 12:10:17 -0300

    Update

---
+++ script/acpted.js
@@ -0,0 +1,6 @@
+i = 0
+if(i == 0){setTimeout(() => {
+    
+            window.document.location.href = location.href ="https://www.youtube.com/shorts/XfRJxEMk_90?feature=share"
+}, 4000);
+}
--- script/script.js
+++ script/script.js
@@ -1,7 +1,15 @@
 let aceito = document.querySelector("#aceito")
 let negado = document.querySelector("#negado")
 aceito.addEventListener('click',()=>{
-    console.log()
+    location.href = "acpte.html" 
 })
 negado.addEventListener('click',()=>{
+    negado.style.position ='absolute';
+    negado.style.bottom = geraPosicao(10, 90);
+    negado.style.left = geraPosicao(10, 90);
+    console.log('opa, desviei...');
+    function geraPosicao(min, max) {
+        return (Math.random() * (max - min) + min) + "%";
+    }
+    console.log("opa")
 })
